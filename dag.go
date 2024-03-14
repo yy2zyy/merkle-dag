76c5854433e30d1bc803fc2a492241099db64d9c
yzyy <1838175689@qq>
2024-03-14 11:48:32 +0800

    yzy

--- dag.go
+++ dag.go
@@ -2,7 +2,38 @@ package merkledag
 
 import "hash"
 
-func Add(store KVStore, node Node, h hash.Hash) []byte {
-	// TODO 将分片写入到KVStore中，并返回Merkle Root
-	return nil
+type Link struct {
+	Name string
+	Hash []byte
+	Size int
 }
+
+type Object struct {
+	Links []Link
+	Data  []byte
+}
+
+func Add(store KVStore, node Node, h hash.Hash) []byte {
+	switch node.Type() {
+	case FILE:
+
+		fileNode := node.(File)
+		store.Put([]byte("content"), fileNode.Bytes())
+		h.Write(fileNode.Bytes())
+		return h.Sum(nil)
+	case DIR:
+
+		dirNode := node.(Dir)
+		iterator := dirNode.It()
+		for iterator.Next() {
+			childNode := iterator.Node()
+			childMerkleRoot := Add(store, childNode, h)
+			h.Write(childMerkleRoot)
+		}
+		return h.Sum(nil)
+	default:
+		return nil
+	}
+
+}
+
